0edb2466e930333584d64cd72b12528f6907a343
anshul 524 <anshul0524@gmail.com>
2025-05-16 11:28:32 +0530

    change

---
+++ app.js
@@ -0,0 +1,8 @@
+const btn = $("#button");
+$(window).on("scroll", () =>
+  btn.toggleClass("show", $(window).scrollTop() > 300)
+);
+btn.on("click", (e) => {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, 300);
+});
